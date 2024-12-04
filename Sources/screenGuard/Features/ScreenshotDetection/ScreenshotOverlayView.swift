d0ecacddd238d639f817341d6f0ea9ba173eef7f
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-12-03 23:37:01 -0500

    passing custom view insetad of the label message on the screenshotoverlayview

--- Sources/screenGuard/Features/ScreenshotDetection/ScreenshotOverlayView.swift
+++ Sources/screenGuard/Features/ScreenshotDetection/ScreenshotOverlayView.swift
@@ -8,26 +8,44 @@
 import UIKit
 
 public class ScreenshotOverlayView: UIView {
-    private let messageLabel: UILabel
+    
+    // This will hold the custom view passed by the developer
+    private var customView: UIView?
 
-    public override init(frame: CGRect) {
-        messageLabel = UILabel()
+    // Init method to accept a custom view
+    public init(frame: CGRect, customView: UIView? = nil) {
+        self.customView = customView
         super.init(frame: frame)
-
-        messageLabel.text = "Oops! Screenshot is not allowed."
-        messageLabel.textColor = .white
-        messageLabel.textAlignment = .center
-        addSubview(messageLabel)
-
-        messageLabel.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            messageLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
-            messageLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
-        ])
-
-        backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        
+        // If no custom view is passed, show a default message
+        if let customView = customView {
+            addSubview(customView)
+            
+            // Set up custom view constraints if needed (center the view)
+            customView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                customView.centerXAnchor.constraint(equalTo: centerXAnchor),
+                customView.centerYAnchor.constraint(equalTo: centerYAnchor)
+            ])
+        } else {
+            // Default behavior: Show the message if no custom view is passed
+            let messageLabel = UILabel()
+            messageLabel.text = "Default behaviour"
+            messageLabel.textColor = .white
+            messageLabel.textAlignment = .center
+            addSubview(messageLabel)
+            
+            messageLabel.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                messageLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+                messageLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+            ])
+            
+            backgroundColor = UIColor.black.withAlphaComponent(0.7)
+        }
     }
 
+    // This is required by the NSCoder when initializing from storyboard (if used)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
