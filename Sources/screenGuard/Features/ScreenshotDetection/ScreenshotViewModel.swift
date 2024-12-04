32ea2793ab6d4bb5b5a0e5b534d8648d99066d98
Lovice Sunuwar <53461002+LoviceSunuwar@users.noreply.github.com>
2024-12-03 23:02:27 -0500

    Added stopobserving function in screenshotVM

--- Sources/screenGuard/Features/ScreenshotDetection/ScreenshotViewModel.swift
+++ Sources/screenGuard/Features/ScreenshotDetection/ScreenshotViewModel.swift
@@ -8,18 +8,21 @@
 import Foundation
 
 public class ScreenshotViewModel {
-    private var screenshotObserver: ScreenshotObserver
+    private var screenshotObserver: ScreenshotObserver?
 
     public var onScreenshotDetected: (() -> Void)?
 
     public init() {
         screenshotObserver = ScreenshotObserver()
-        screenshotObserver.onScreenshotDetected = { [weak self] in
-            self?.onScreenshotDetected?()
-        }
     }
 
     public func startObserving() {
-        screenshotObserver.startObserving()
+        screenshotObserver?.registerObserver {
+            self.onScreenshotDetected?()
+        }
+    }
+
+    public func stopObserving() {
+        screenshotObserver?.removeObserver()
     }
 }
